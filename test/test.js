38c111a5e0a9f878e36020191cb3efa44e32701e
Amalie Oxholm <amalieoxholm@Amalies-MacBook-Pro-2.local>
2020-12-10 23:38:15 +0100

    Final userinterface changes

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@
 var server = require('../backend/Server/API')
 var chai = require('chai');
 var chaiHTTP = require('chai-http');
-
 //assertion style
 chai.should()
 chai.use(chaiHTTP);
+var expect = require('chai').expect;
 
 describe('API', function (){
     it('It should post a new like', (done) => {
@@ -19,6 +19,9 @@ describe('API', function (){
         .end((err,response)=> {
             response.should.have.status(200);
             response.should.be.a('object');
+            expect("Erik").to.be.a('string');
+            expect({ username: 'Ida' }).to.be.an('object');
+
             done()
         });
     });
@@ -36,6 +39,8 @@ describe('API', function (){
         .end((err,response)=> {
             response.should.have.status(200);
             response.should.be.a('object');
+            expect("Ida").to.be.a('string');
+            expect({ username: 'Erik' }).to.be.an('object');
             done()
         });
     });
